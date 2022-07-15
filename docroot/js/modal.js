4150adc7fc544420599e236cb63d1ce3d4bc7976
Reptoxx2 <ilya.naik11@gmail.com>
2022-07-15 09:43:59 +0300

    first

---
+++ docroot/js/modal.js
@@ -0,0 +1,6 @@
+let modal = document.querySelectorAll('.modal');
+let close = document.getElementById('close-but')
+let send = document.getElementById('send-but')
+let connect = document.getElementById('connection')
+
+
