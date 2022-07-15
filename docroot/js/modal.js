4eaddf74fa0169b62e0812529236ddb1353581da
Reptoxx2 <ilya.naik11@gmail.com>
2022-07-15 10:29:09 +0300

    twice

--- docroot/js/modal.js
+++ docroot/js/modal.js
@@ -1,6 +1,17 @@
-let modal = document.querySelectorAll('.modal');
+let modal = document.querySelector('.modal');
+let modalc = document.querySelector('.modal__content');
 let close = document.getElementById('close-but')
 let send = document.getElementById('send-but')
 let connect = document.getElementById('connection')
 
+document.body.addEventListener("click", (event) => {
 
+  if (event.target === connect)
+  {
+    modal.classList.remove('active');
+  }
+  else if (!event.target.closest('.modal__wrap') || event.target === close)
+  {
+    modal.classList.add('active');
+  }
+})
